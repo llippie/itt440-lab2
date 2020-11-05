a4bd434c2888d6d58eedb5204e9887eaf350994c
llippie <syafie_tugiman2@yahoo.com>
2020-11-05 13:23:52 +0000

    Before 2.4

--- 2.1.c
+++ 2.1.c
@@ -1,1 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <sys/types.h>
+//#include <unistd.h>
+
+int main()
+{
+ fork();
+ printf("Hello World!\n");
+ return 0;
+}
---
+++ 2.3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void childTask()
+{
+ printf("Salam, saya anak\n");
+}
+void parentTask()
+{
+ printf("Saya pula bapaknya \n");
+}
+int main(void)
+{
+ pid_t pid=fork();
+ 
+ if(pid ==0)
+ {
+  childTask();
+  exit(EXIT_SUCCESS);
+ }
+ else if(pid >0)
+ {
+  wait(NULL);
+  parentTask();
+ }
+ else
+ {
+  printf("Unable to create child process.\n");
+ }
+return EXIT_SUCCESS;
+}
